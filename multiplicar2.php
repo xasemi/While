3a99cb8b24451e7b9f149f11bdd916458aebe24e
xasemi <xavism.1997@gmail.com>
2016-10-24 19:52:15 +0200

    primer commit

---
+++ multiplicar2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+	<title> Tabla de multiplicar </title>
+</head>
+<body>
+	<H1> Tabla del 2</H1>
+	<?php
+		$i=1;
+		while ($i<=10) {
+			$u=$i*2;
+			echo "2*$i=$u <br>";
+			$i++;
+		}
+	 ?>
+</body>
+</html>
